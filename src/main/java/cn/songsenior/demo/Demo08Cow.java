ebe25815c039aa3b736b076f70ef8f6194a5d3e6
Kimetiii <songsenior@foxmail.com>
2020-07-09 12:26:47 +0800

    Remark:奶牛

--- src/main/java/cn/songsenior/demo/Demo08Cow.java
+++ src/main/java/cn/songsenior/demo/Demo08Cow.java
@@ -1,7 +1,5 @@
 package cn.songsenior.demo;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,62 +10,25 @@ import java.util.Scanner;
  */
 public class Demo08Cow {
 	public static void main(String[] args) {
-		//输入所要计算的年
 		Scanner sc = new Scanner(System.in);
-		//测试用 存放每只牛的年龄
-		List<Integer> rm = new ArrayList<Integer>();
-		//定义一个集合存放牛的数量
-		List<Animal> m = new ArrayList<Animal>();
-		m.add(new Animal(1));
-		//判断查询哪一年的数量
 		int n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
+		System.out.println(getAmount(n));
+	}
 
-			for (int j = 0; j < m.size(); j++) {
-				Animal animal = m.get(j);
-				//看看牛是否可以生育
-				if (animal.getAge() >= 3 && animal.getAge() <= 7) {
-					m.add(new Animal(1));
-				}
-				//判断牛是否死亡
-				if (animal.getAge() == 10) {
-					m.remove(j);
-				} else {
-					//年龄加
-					animal.setAge(animal.getAge() + 1);
-				}
-			}
-			rm.add(m.size());
-			//测试用输出当年牛的年龄
-//            if(i==n){
-//                Iterator it = m.iterator();
-//                while (it.hasNext()) {
-//                    Animal animal = (Animal)it.next();
-//                    System.out.println(animal.getAge()-1);
-//                }
-//            }
+	private static int getAmount(int n) {
+		int first = 1;
+		int second = 2;
+		int last = 3;
+		if (n <= 3) {
+			return n;
 		}
-
-		//输出牛的数量
-		System.out.println(m.size());
+		for (int i = 0; i < n; i++) {
+			int temp = first + last;
+			first = second;
+			second = last;
+			last = temp;
+		}
+		return last;
 	}
-
 }
 
-//定义母牛这个类
-class Animal {
-	private int age;
-
-	public Animal(int age) {
-		this.age = age;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-}
