4c90035c0397585cc87c307871610e0d84bf283b
Kimetiii <songsenior@foxmail.com>
2020-07-10 20:47:15 +0800

    Remark:连续整数之和

--- src/main/java/cn/songsenior/demo/Demo01SumInt.java
+++ src/main/java/cn/songsenior/demo/Demo01SumInt.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 /**
  * 连续整数之和
+ * 一个正整数有可能可以被表示为 m(m>1)个连续正整数之和：15=1+2+3+4+5
+ * 判断给定的整数 n 能否表示成连续的 m(m>1)个正整数之和
+ * <p>
+ * 示例：
+ * 输入：15
+ * 输出：YES
  */
 public class Demo01SumInt {
 	public static void main(String[] args) {
@@ -20,6 +26,7 @@ public class Demo01SumInt {
 		}
 		System.out.println(flag);
 	}
+
 	public static int addNum(int n, int min) {
 		if (n == min)
 			return min;
